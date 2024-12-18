7aaf8a08c6e9ff2b455b0c2acc3690eaee8a1e1a
Jonathan <jonathan.s9522@gmail.com>
2024-12-18 02:54:25 -0500

    add module file because ng 17 seems to not do it by default now

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,7 @@
+import {NgModule} from '@angular/core';
+import { FormComponent } from './form/form.component';
+
+@NgModule({
+  declarations: [FormComponent]
+})
+export class CustomMenuModule { }
