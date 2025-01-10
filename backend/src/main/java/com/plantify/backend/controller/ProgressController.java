bcb7d78db3f7929ba95140616beb4651993aab72
Jonathan <jonathan.s9522@gmail.com>
2025-01-10 01:23:56 -0500

    final commit for ang fe

--- backend/src/main/java/com/plantify/backend/controller/ProgressController.java
+++ backend/src/main/java/com/plantify/backend/controller/ProgressController.java
@@ -5,10 +5,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/progress")
 public class ProgressController {
+    private String formData;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setFormData(String data) {
+        this.formData = data;
+    }
 
     // this progress conttroller will be used for handling progress related endpoints. example is the submit progress endpoint
     @PostMapping
-    public String submitProgress(@RequestParam String progressDetail) {
+    public String submitProgress(@RequestBody String progressDetail) {
+        setFormData(progressDetail);
+        //do something different with the data if submitting vs saving when db is setup
+        if (this.status == "submit") {
+
+        }
         return "Progress submitted: " + progressDetail;
     }
 }
