0c36c385220b886d3d7e4f8d9f1a78336aad00ec
Jonathan <jonathan.s9522@gmail.com>
2025-01-13 04:39:01 -0500

    add home page skeleton

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,9 +1,32 @@
 import styles from './home.module.css';
+import {Link} from 'react-router-dom';
 
 export default function HomePage() {
+    const Plant = () => {
+        return (
+            <div className={styles.plant_container}>
+                <div className={styles.shadow}></div>
+                <div className={styles.plant}>
+                    <div className={styles.leaf1}></div>
+                    <div className={styles.leaf2}></div>
+                    <div className={styles.leaf3}></div>
+                    <div className={styles.stem}></div>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div>
+        <div className = {styles.home_container}>
             <h1>Home</h1>
+            <Plant/>
+            <p>Welcome!</p>
+            <Link to = '/report'>
+                <button className = {styles.nav_buttons}>Write Report</button>
+            </Link>
+            <Link to = '/profile'>
+                <button className = {styles.nav_buttons}>My Profile</button>
+            </Link>
         </div>
     )
 }
